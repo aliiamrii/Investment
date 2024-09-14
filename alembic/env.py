b49246bd08154e1ebf25afcbdfa20d523e86288e
Ali Amri <ali@Alis-MacBook-Pro.local>
2024-09-14 17:47:40 +0330

    the investment table is updated.

--- alembic/env.py
+++ alembic/env.py
@@ -1,12 +1,15 @@
 from logging.config import fileConfig
-from app.models import db
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 
+from app.models import db  # Adjust according to your app structure
+print(db.metadata.tables)
 target_metadata = db.metadata
+
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-#
+# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
